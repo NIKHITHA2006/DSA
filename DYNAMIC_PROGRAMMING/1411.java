9b3e28bda30fd02ee5f3a72c750aa9c11dbe9bbd
NIKHITHA2006 <nimmicdm@gmail.com>
2026-01-03 11:48:11 +0530

    Initial commit

---
+++ DYNAMIC_PROGRAMMING/1411.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numOfWays(int n) {
+        long MOD= 1000000007;
+        long abc=6;
+        long aba=6;
+        for(int i=2;i<=n;i++)
+        {
+            long nabc=(2*abc+2*aba)%MOD;
+            long naba=(2*abc+3*aba)%MOD;
+            abc=nabc;
+            aba=naba;
+        }
+        return (int)((aba+abc)%MOD);
+    }
+}
