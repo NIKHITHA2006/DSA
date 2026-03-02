d77f4563f2dd313c05316d96c74c6793eadfe5bf
NIKHITHA M <147826971+NIKHITHA2006@users.noreply.github.com>
2026-03-02 23:23:39 +0530

    added solutions

---
+++ ARRAY/122.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l=prices.length;
+        int t=0;
+        for(int i=1;i<l;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                t+=prices[i]-prices[i-1];
+            }
+        }
+        return t;
+    }
+}
