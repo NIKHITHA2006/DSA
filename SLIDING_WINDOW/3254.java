377021ea9e9c48541b7aa59d5b520b07c0020cce
NIKHITHA M <147826971+NIKHITHA2006@users.noreply.github.com>
2026-03-05 23:43:41 +0530

    added solutions

--- SLIDING_WINDOW/3254.java
+++ SLIDING_WINDOW/3254.java
@@ -1,1 +1,31 @@
 
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+
+ 
+        if (k == 1) return nums.clone();
+
+        int[] result = new int[n - k + 1];
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+
+        
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (i >= k - 1) {
+                if (count >= k) {
+                    result[i - k + 1] = nums[i]; 
+                } else {
+                    result[i - k + 1] = -1;
+                }
+            }
+        }
+        return result;
+    }
+}
