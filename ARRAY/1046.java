53c0873261f6f2f8b89df0bc660bed578e505450
NIKHITHA M <147826971+NIKHITHA2006@users.noreply.github.com>
2026-03-15 19:16:31 +0530

    added
    Implement lastStoneWeight method to calculate the weight of the last stone.

--- ARRAY/1046.java
+++ ARRAY/1046.java
@@ -1,1 +1,49 @@
 
+import java.util.*;
+
+public class Main {
+
+    public static int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int stone : stones)
+        {
+            pq.add(stone);
+        }
+
+        while(pq.size() > 1)
+        {
+            int y = pq.poll(); 
+            int x = pq.poll(); 
+
+            if(y != x)
+            {
+                pq.add(y - x);
+            }
+        }
+
+        if(pq.isEmpty())
+            return 0;
+        else
+            return pq.poll();
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] stones = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            stones[i] = sc.nextInt();
+        }
+
+        int result = lastStoneWeight(stones);
+
+        System.out.println(result);
+    }
+}
