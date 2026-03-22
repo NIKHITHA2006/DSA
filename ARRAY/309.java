77e53760809d1a9c16064b48e51847ee91f6e8e3
NIKHITHA M <147826971+NIKHITHA2006@users.noreply.github.com>
2026-03-22 19:38:00 +0530

    added solutions

--- ARRAY/309.java
+++ ARRAY/309.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0)
+        {
+            return 0;
+        }
+        int hold=-prices[0];
+        int s=0;
+        int r=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            int phold=hold;
+            int ps=s;
+            int pr=r;
+            hold=Math.max(phold,pr-prices[i]);
+            s=phold+prices[i];
+            r=Math.max(pr,ps);
+        }
+        return Math.max(r,s);
+    }
+}
