7d5b5acfdb0acdc0141bf69beadcaf0d4bfc2a2d
NIKHITHA M <147826971+NIKHITHA2006@users.noreply.github.com>
2026-06-16 22:20:50 +0530

    Create 3612.java

---
+++ STRING/3612.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String processStr(String s) {
+                StringBuilder res = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isLowerCase(ch)) {
+                res.append(ch);
+            }
+
+            else if (ch == '*') {
+                if (res.length() > 0) {
+                    res.deleteCharAt(res.length() - 1);
+                }
+            }
+
+            else if (ch == '#') {
+                res.append(res.toString());
+            }
+
+            else if (ch == '%') {
+                res.reverse();
+            }
+        }
+
+        return res.toString();
+
+        
+    }
+}
